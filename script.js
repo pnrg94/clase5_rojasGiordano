340dab0eb0fc7bc9b54df43bea48960db806941f
pnrg94 <pnrg94@gmail.com>
2021-10-19 13:45:15 -0300

    nuevos ajustes

--- script.js
+++ script.js
@@ -1,44 +1,3 @@
-/*class libro{
-    constructor (titulo, autor, editorial, paginas, ejemplares){
-        this.titulo = titulo;
-        this.autor = autor;
-        this.editorial = editorial;
-        this.paginas = parseInt(paginas);
-        this.ejemplares = parseInt(ejemplares);
-    }
-
-    retirar(cantidadRetirar){
-        if (this.verificarDisponibilidad(cantidadRetirar)){
-            this.ejemplares = this.ejemplares - cantidadRetirar;
-            alert ("Se retiraron " + cantidadRetirar + " ejemplares de " + this.titulo + ".");
-            alert ("Quedan aún " + this.ejemplares + " disponibles.");
-        }else{
-            alert("No hay suficientes ejemplares para retirar. Tenemos a disposición: " + this.ejemplares + " ejemplares.");
-        }
-    }
-
-    verificarDisponibilidad(cantidadRetirar){
-        if(this.ejemplares >= cantidadRetirar){
-            return true;
-        }
-    }
-}
-
-const libro1 = new libro("El Aleph", "Jorge Luis Borges", "Zahir", 146, 5);
-const libro2 = new libro("Rayuela", "Julio Cortazar", "Alfaguara", 908, 3);
-const libro3 = new libro("El amor en los tiempos del cólera", "Gabriel García Márquez", "Zahir", 442, 4);
-const libro4 = new libro("Sobre héroes y tumbas", "Ernesto Sabato", "Zahir", 386, 1);
-const libro5 = new libro("La invención de Morel", "JAdolfo Bioy Casares", "Zahir", 96, 0);
-
-for (const propiedades in libro2){
-    alert(propiedades + ": " + libro2[propiedades]);
-}
-
-let retirar = prompt("¿Cuantos ejemplares desea retirar?");
-
-libro2.retirar(retirar);*/
-
-
 class libro{
     constructor (titulo, autor, editorial, ejemplares){
         this.titulo = titulo;
@@ -46,20 +5,15 @@ class libro{
         this.editorial = editorial;
         this.ejemplares = parseInt(ejemplares);
     }
-    
     retirar(cantidadRetirar){
-        if (this.verificarDisponibilidad(cantidadRetirar)){
-            this.ejemplares = this.ejemplares - cantidadRetirar;
-            alert ("Se retiraron " + cantidadRetirar + " ejemplares de " + this.titulo + ". Quedan aún " + this.ejemplares + " disponibles.");
+        if (this.ejemplares >= cantidadRetirar){
+            alert ("Se retiraron " + cantidadRetirar + " ejemplares de " + this.titulo + ". Quedan aún " + this.verificarStock(cantidadRetirar) + " disponibles.");
         }else{
             alert("No hay suficientes ejemplares para retirar. Tenemos a disposición: " + this.ejemplares + " ejemplares.");
         }
     }
-
-    verificarDisponibilidad(cantidadRetirar){
-        if(this.ejemplares >= cantidadRetirar){
-            return true;
-        }
+    verificarStock(cantidadRetirar){
+        return this.ejemplares - cantidadRetirar
     }
 }
 
@@ -73,42 +27,37 @@ Seleccione su producto:
     5. La invención de Morel
 `));
 
-const retirar = prompt("¿Cuantos ejemplares desea retirar?");
+const cantidadRetirar = prompt("¿Cuantos ejemplares desea retirar?");
 
 switch(libros){
     case 1:
         const libro1 = new libro("El Aleph", "Jorge Luis Borges", "Zahir", 5);
         alert("Eligió " + libro1.titulo + " (" + libro1.autor + "). Editorial " + libro1.editorial + ". Hay disponibles " + libro1.ejemplares + " ejemplares para retirar.");
-        alert = retirar;
-        libro1.retirar(retirar)
+        libro1.retirar(cantidadRetirar)
         break;
 
     case 2:
         const libro2 = new libro("Rayuela", "Julio Cortazar", "Alfaguara", 3);;
         alert("Eligió " + libro2.titulo + " (" + libro2.autor + "). Editorial " + libro2.editorial + ". Hay disponibles " + libro2.ejemplares + " ejemplares para retirar.");
-        alert = retirar;
-        libro2.retirar(retirar)
+        libro2.retirar(cantidadRetirar)
         break;
 
     case 3:
         const libro3 = new libro("El amor en los tiempos del cólera", "Gabriel García Márquez", "Random House", 4);;
         alert("Eligió " + libro3.titulo + " (" + libro3.autor + "). Editorial " + libro3.editorial + ". Hay disponibles " + libro3.ejemplares + " ejemplares para retirar.");
-        alert = retirar;
-        libro3.retirar(retirar)
+        libro3.retirar(cantidadRetirar)
         break;
     
     case 4:
         const libro4 = new libro("Sobre héroes y tumbas", "Ernesto Sabato", "La Nación", 1);;
         alert("Eligió " + libro4.titulo + " (" + libro4.autor + "). Editorial " + libro4.editorial + ". Hay disponibles " + libro4.ejemplares + " ejemplares para retirar.");
-        alert = retirar;
-        libro4.retirar(retirar)
+        libro4.retirar(cantidadRetirar)
         break;
 
     case 5:
         const libro5 = new libro("La invención de Morel", "Adolfo Bioy Casares", "Booket", 0);;
         alert("Eligió " + libro5.titulo + " (" + libro5.autor + "). Editorial " + libro5.editorial + ". Hay disponibles " + libro5.ejemplares + " ejemplares para retirar.");
-        alert = retirar;
-        libro5.retirar(retirar)
+        libro5.retirar(cantidadRetirar)
         break;
 
     default:
